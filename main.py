52b690e95797381a861d9c5e8994e93b1c675ade
Callum Mackay <mackaycallum99@gmail.com>
2018-03-20 19:45:08 +0000

    Added different K scaling for new players and top players

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ class Player:
     ELO = 1200
     expected = 0
     result = 0
-    k = 32
     wins = 0
     losses = 0
     games = 0
@@ -48,6 +47,19 @@ def GetPlayers():
         players.append(x)
         #print(x.name)
 
+def GetK(player):
+    newPlayerGames = 5
+    newPlayerK = 40
+    midPlayerK = 20
+    topPlayerELO = 1400
+    topPlayerK = 10
+    if player.games > newPlayerGames:
+        return newPlayerK
+    elif player.ELO > topPlayerELO:
+        return topPlayerK
+    else:
+        return midPlayerK
+
 def match(challenger, defendant, winner):
     # Calculate the expected score for both players
     challenger.expected = expected(challenger.ELO, defendant.ELO)
@@ -67,8 +79,8 @@ def match(challenger, defendant, winner):
     challenger.games += 1
     defendant.games += 1
     # Set new ELO ratings
-    challenger.ELO = elo(challenger.ELO, challenger.expected, challenger.result, k=challenger.k)
-    defendant.ELO = elo(defendant.ELO, defendant.expected, defendant.result, k=defendant.k)
+    challenger.ELO = elo(challenger.ELO, challenger.expected, challenger.result, GetK(challenger))
+    defendant.ELO = elo(defendant.ELO, defendant.expected, defendant.result, GetK(defendant))
 
 def CalculateELO():
     ResetCSV()
